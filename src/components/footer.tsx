10a983182b98ce8112fd796419c761c9a48a5284
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2023-08-04 17:00:02 -0600

    up and away

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,27 +4,27 @@ import { FaFacebook, FaYoutube, FaInstagram } from "react-icons/fa";
 export const Footer = () => {
     return (
         <VStack p={16} w="full" backgroundColor="#1E1E1E">
-            <VStack w="full" maxW="1080px">
+            <VStack w="full" maxW="1080px" mb="15px">
                 {/* <Image src="/legal-logo.svg" /> */}
-                <Divider mt={"64px"} mb={"48px"} color="#f2f0ee" />
+                <Divider my={"32px"} color="#f2f0ee" />
                 <Text color="#f2f0ee" fontSize={"14px"}>
                     aude, hospes, contemnere opes et te quoque dignum | finge
                     deo, rebusque veni non asper egenis. (Aen. 8. 364-5)
                 </Text>
-                <Divider mt={"64px"} mb={"48px"} color="#f2f0ee" />
+                <Divider my={"32px"} color="#f2f0ee" />
                 <Grid w="full" gridTemplateColumns="1fr 1fr 1fr">
                     <HStack>
                         <Icon as={FaYoutube} color="#f2f0ee" />
                         <Icon as={FaFacebook} color="#f2f0ee" />
                         <Icon as={FaInstagram} color="#f2f0ee" />
                     </HStack>
-                    <VStack spacing={18} align="center">
+                    <VStack align="center">
                         <Text
                             fontSize={"10px"}
                             color="#f2f0ee"
                             align={"center"}
                         >
-                            Condiciones de uso | Política de privacidad
+                            <i>Condiciones de uso | Política de privacidad</i>
                         </Text>
                     </VStack>
                     <Text color="#f2f0ee" fontSize={"10px"} align={"end"}>
