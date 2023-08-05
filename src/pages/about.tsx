c31ec672300f77394e5ab3a95396f178e39e8b9a
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2023-08-04 20:33:49 -0600

    up and away

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -20,19 +20,24 @@ export const About = () => {
                         eneas
                     </Text>
                     <Divider w={32} color={"#E7E4D8"} />
-                    <Text color={"#E7E4D8"} fontSize={14}>
-                        Eneas es el contrato de arrendamiento que hasta un
-                        cavernícola puede configurar de forma sencilla,
-                        profesional y segura. Déjate llevar por nuestra potente
-                        tecnología y da forma a tu contrato ideal para arrendar
-                        fincas habitacionales, comerciales, industriales o
-                        rústicas. Recuerda, basta con que respondas unas cuantas
-                        preguntas para que el sistema de Patrono te ayude a
-                        generar —en línea y de manera inmediata— un contrato
-                        único adaptado tanto a tus necesidades como a las
-                        regulaciones federales y estatales que a tu caso le sean
-                        aplicables.
-                    </Text>
+                    <VStack spacing={4}>
+                        <Text color={"#E7E4D8"} fontSize={16}>
+                            <i>Eneas</i> © es el contrato de arrendamiento que hasta un
+                            cavernícola puede configurar de forma sencilla,
+                            profesional y segura. Déjate llevar por nuestra
+                            potente tecnología y da forma a tu contrato ideal
+                            para arrendar fincas habitacionales, comerciales,
+                            industriales o rústicas.
+                        </Text>
+                        <Text color={"#E7E4D8"} fontSize={16}>
+                            Recuerda, basta con que respondas unas cuantas
+                            preguntas para que el sistema de Patrono te ayude a
+                            generar —en línea y de manera inmediata— un contrato
+                            único adaptado tanto a tus necesidades como a las
+                            regulaciones federales y estatales que a tu caso le
+                            sean aplicables.
+                        </Text>
+                    </VStack>
                     <VStack align={"start"}>
                         <Text fontSize={32} color={"#FA7200"} fontWeight={700}>
                             GRATIS
