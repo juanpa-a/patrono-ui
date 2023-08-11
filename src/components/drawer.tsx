717514a244db0c9147aae67491e835bbb74a24d9
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2023-08-11 16:16:49 -0600

    up and away

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -6,6 +6,7 @@ import {
     VStack,
     Center,
     Image,
+    Box,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
@@ -34,54 +35,58 @@ export const Drawer = ({ isOpen, onClose }: Props) => {
         };
     }, []);
     return (
-        <ChakraDrawer placement={"left"} onClose={onClose} isOpen={isOpen}>
-            <DrawerOverlay />
-            <DrawerContent 
-                // bg={"#E7E4D8"}
-                backgroundColor={
-                    isScrolled ? "rgba(224, 221, 207, 0.85)" : "rgba(224, 221, 207, 0.95)"
-                }
-                transition="background-color 0.4s ease"
-            >
-                <DrawerBody
-                    display={"flex"}
-                    flexDirection={"column"}
-                    justifyContent={"center"}
-                    alignContent={"center"}
-                    gap={"128px"}
+        <Box overflow={"hidden"}>
+            <ChakraDrawer placement={"left"} onClose={onClose} isOpen={isOpen}>
+                <DrawerOverlay />
+                <DrawerContent
+                    // bg={"#E7E4D8"}
+                    backgroundColor={
+                        isScrolled
+                            ? "rgba(224, 221, 207, 0.85)"
+                            : "rgba(224, 221, 207, 0.95)"
+                    }
+                    transition="background-color 0.4s ease"
                 >
-                    <motion.div whileHover={{ scale: 1.2 }}>
-                        <VStack
-                            alignContent="center"
-                            onClick={() => {
-                                navigate("/create");
-                                onClose();
-                            }}
-                        >
-                            <Image src="/desk.png" />
-                            <Center mt={3}>ESCRITORIO</Center>
-                        </VStack>
-                    </motion.div>
-                    <motion.div whileHover={{ scale: 1.2 }}>
-                        <VStack
-                            alignContent="center"
-                            onClick={() => {
-                                navigate("/favorites");
-                                onClose();
-                            }}
-                        >
-                            <Image src="/arch.png" />
-                            <Center mt={3}>ARCHIVO</Center>
-                        </VStack>
-                    </motion.div>
-                    <motion.div whileHover={{ scale: 1.2 }}>
-                        <VStack alignContent="center">
-                            <Image src="/obs.png" />
-                            <Center mt={3}>OBSERVATORIO</Center>
-                        </VStack>
-                    </motion.div>
-                </DrawerBody>
-            </DrawerContent>
-        </ChakraDrawer>
+                    <DrawerBody
+                        display={"flex"}
+                        flexDirection={"column"}
+                        justifyContent={"center"}
+                        alignContent={"center"}
+                        gap={"128px"}
+                    >
+                        <motion.div whileHover={{ scale: 1.2 }}>
+                            <VStack
+                                alignContent="center"
+                                onClick={() => {
+                                    navigate("/create");
+                                    onClose();
+                                }}
+                            >
+                                <Image src="/desk.png" />
+                                <Center mt={3}>ESCRITORIO</Center>
+                            </VStack>
+                        </motion.div>
+                        <motion.div whileHover={{ scale: 1.2 }}>
+                            <VStack
+                                alignContent="center"
+                                onClick={() => {
+                                    navigate("/favorites");
+                                    onClose();
+                                }}
+                            >
+                                <Image src="/arch.png" />
+                                <Center mt={3}>ARCHIVO</Center>
+                            </VStack>
+                        </motion.div>
+                        <motion.div whileHover={{ scale: 1.2 }}>
+                            <VStack alignContent="center">
+                                <Image src="/obs.png" />
+                                <Center mt={3}>OBSERVATORIO</Center>
+                            </VStack>
+                        </motion.div>
+                    </DrawerBody>
+                </DrawerContent>
+            </ChakraDrawer>
+        </Box>
     );
 };
